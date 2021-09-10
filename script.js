7e7258e453c6802f77a1450e5bff3bac476fb9c8
jacksonrodri <jacksonrodriguez329@gmail.com>
2021-09-10 17:44:23 -0400

    Change $

--- script.js
+++ script.js
@@ -223,7 +223,7 @@ const updatePageForHash = () => {
               }
               
               newOrderItem.querySelector(".product-name").innerText = item.name;
-              newOrderItem.querySelector(".product-price-text.smaller").innerText = '$ ${item.price}';
+              newOrderItem.querySelector(".product-price-text.smaller").innerText = `${item.price}`;
               newOrderItem.querySelector(".quantity.tiny-left-margin").innerText = item.quantity;
               newOrderWrapper.appendChild(newOrderItem);
             });
